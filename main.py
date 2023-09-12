1b1516efe062c51c97e70d220e9d052ab041dbfe
Serhii <137983370+Serhii456@users.noreply.github.com>
2023-09-12 10:48:29 +0200

    Дз8

---
+++ main.py
@@ -0,0 +1,36 @@
+from datetime import date, timedelta, datetime
+
+birthday_dict = {}
+
+def get_birthdays_per_week(users):
+    if not users:
+        birthday_dict.clear()
+    else:
+        today = date.today()
+        end_day = today + timedelta(days=7)
+        for user in users:
+            birthday = user['birthday']
+            if birthday.month == 1:
+                birthday = birthday.replace(year=today.year + 1)
+            else:
+                birthday = birthday.replace(year=today.year)
+            if today <= birthday <= end_day:
+                day_of_week = birthday.strftime("%A")
+                if day_of_week in ["Saturday", "Sunday"]:
+                    birthday_dict.setdefault('Monday', []).append(user['name'])
+                else:
+                    birthday_dict.setdefault(day_of_week, []).append(user['name'])
+    return birthday_dict
+   
+
+
+if __name__ == "__main__":
+    users = [
+        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+    ]
+
+    result = get_birthdays_per_week(users)
+    print(result)
+    # Виводимо результат
+    for day_name, names in result.items():
+        print(f"{day_name}: {', '.join(names)}")
